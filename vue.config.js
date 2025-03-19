1672d0ebe4aeb2e623a04c8a3eeb83ddce54bebd
Thiago Mingorance <out1610@gmail.com>
2025-03-19 19:30:34 -0300

    Descrição das alterações

--- vue.config.js
+++ vue.config.js
@@ -1,8 +1,6 @@
 const { defineConfig } = require('@vue/cli-service');
 module.exports = defineConfig({
   transpileDependencies: true,
-  publicPath: '/', // Caminho base para produção e desenvolvimento
-  outputDir: 'dist', // Pasta de saída do build
-  assetsDir: 'static', // Pasta para assets (opcional)
-  indexPath: 'index.html', // Arquivo principal (opcional)
+  
+  publicPath: '/',
 });
